91c92ba7b0e038acadb4e301cda8b6412eca8998
Deb St. Cyr <156037147+14dstcyr@users.noreply.github.com>
2024-04-03 12:19:58 -0500

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,27 +1,10 @@
-# --------------------------------------------
-# Imports at the top - PyShiny EXPRESS VERSION
-# --------------------------------------------
-
-# From shiny, import just reactive and render
 from shiny import reactive, render
-
-# From shiny.express, import just ui
 from shiny.express import ui
-
-# Imports from Python Standard Library to simulate live data
 import random
 from datetime import datetime
-
-# --------------------------------------------
-# Import icons as you like
-# --------------------------------------------
-
 from faicons import icon_svg
 
-# --------------------------------------------
-# FOR LOCAL DEVELOPMENT
-# --------------------------------------------
-# Add all packages not in the Std Library
+Add all packages not in the Std Library
 # to requirements.txt:
 #
 # faicons
